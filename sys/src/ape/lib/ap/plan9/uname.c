e0c64c59d306826b9020f350fe6ff08a0eb08778
David du Colombier <0intro@gmail.com>
2006-03-29 08:41:33 +0200

    patch: applied/uname-configure
    make uname() return values that gnu configure can understand
    
    Notes:
    Wed Mar 29 17:46:04 EST 2006 rsc
        I put most of the changes in but there
        is no way I am going to make uname
        claim to be a Linux system.
    
        As a compromise, I changed it to check $osname
        and use that if it's set.
    
        You do know that you can run
    
        	./configure --host-i386-pc-linux
    
        or such, right?
    
    Reference: /n/sources/patch/applied/uname-configure
    Date: Wed Mar 29 08:41:33 CES 2006
    Signed-off-by: geoff@collyer.net
    Reviewed-by: rsc

--- sys/src/ape/lib/ap/plan9/uname.c
+++ sys/src/ape/lib/ap/plan9/uname.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
+#include <string.h>
 #include <sys/utsname.h>
 
 int
 uname(struct utsname *n)
 {
-	n->sysname = "Plan9";
+	// n->sysname = "Plan9";
+	n->sysname = "Linux";			/* to make gnu configure work */
 	n->nodename = getenv("sysname");
 	if(!n->nodename){
 		n->nodename = getenv("site");
 		if(!n->nodename)
 			n->nodename = "?";
 	}
-	n->release = "1";
+	n->release = "4";			/* edition */
 	n->version = "0";
-	n->machine = getenv("terminal");
-	if(!n->machine){
-		n->machine = getenv("cputype");
-		if(!n->machine)
-			n->machine = "?";
-	}
+	n->machine = getenv("cputype");
+	if (strcmp(n->machine, "386") == 0)
+		n->machine = "i386";		/* to make gnu configure work */
+	if(!n->machine)
+		n->machine = "?";
 	return 0;
 }
