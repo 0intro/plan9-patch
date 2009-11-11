b36a7e3f70d6497907d4f408be60d7854d0db8d2
David du Colombier <0intro@gmail.com>
2009-11-11 23:06:17 +0100

    patch: maybe/date-iso8601
    Allow printing the date in compact ISO 8601 format. E.g.:
    
    date    :	Wed Nov 11 14:55:04 MST 2009
    date -i :	20091111T145504
    
    The 8601 format allows rc scripts to create compact timestamps for log
    entries, is easier to manipulate with sed/grep/etc., and is a natural
    key for sort(1). ('date -n' is compact and sortable, but isn't human
    readable or regexp friendly.)
    
    Reference: /n/sources/patch/maybe/date-iso8601
    Date: Wed Nov 11 23:06:17 CET 2009
    Signed-off-by: lyndon@orthanc.ca

--- sys/src/cmd/date.c
+++ sys/src/cmd/date.c
@@ -1,17 +1,20 @@
 #include <u.h>
 #include <libc.h>
 
-int uflg, nflg;
+int iflg, nflg, uflg;
 
 void
 main(int argc, char *argv[])
 {
 	ulong now;
+	int tzhr, tzmin;
+	struct Tm *tm;
 
 	ARGBEGIN{
+	case 'i':	iflg = 1; break;
 	case 'n':	nflg = 1; break;
 	case 'u':	uflg = 1; break;
-	default:	fprint(2, "usage: date [-un] [seconds]\n"); exits("usage");
+	default:	fprint(2, "usage: date [-inu] [seconds]\n"); exits("usage");
 	}ARGEND
 
 	if(argc == 1)
@@ -21,6 +24,17 @@ main(int argc, char *argv[])
 
 	if(nflg)
 		print("%ld\n", now);
+	else if(iflg){
+		if(uflg)
+			tm = gmtime(now);
+		else
+			tm = localtime(now);
+		tzhr = tm->tzoff / 3600;
+		tzmin = (abs(tm->tzoff) - (abs(tzhr) * 3600)) / 60;
+		print("%0.4d%0.2d%0.2dT%0.2d%0.2d%0.2d%+.2d%.2d\n",
+			tm->year + 1900, tm->mon + 1, tm->mday,
+			tm->hour, tm->min, tm->sec, tzhr, tzmin);
+	}
 	else if(uflg)
 		print("%s", asctime(gmtime(now)));
 	else
