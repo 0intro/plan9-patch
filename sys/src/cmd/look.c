51e6cae4973d8e35c350dccf5a6490fbba805ac8
David du Colombier <0intro@gmail.com>
2009-11-28 03:43:03 +0100

    patch: applied/lookeargf
    use EARGF when parsing -t flag.
    
    Reference: /n/sources/patch/applied/lookeargf
    Date: Sat Nov 28 03:43:03 CET 2009
    Signed-off-by: quanstro@quanstro.net

--- sys/src/cmd/look.c
+++ sys/src/cmd/look.c
@@ -67,6 +67,13 @@ void	rcanon(Rune*, Rune*);
 int	ncomp(Rune*, Rune*);
 
 void
+usage(void)
+{
+	fprint(2, "usage: %s -[dfinx] [-t c] [string] [file]\n", argv0);
+	exits("usage");
+}
+
+void
 main(int argc, char *argv[])
 {
 	int n;
@@ -88,15 +95,13 @@ main(int argc, char *argv[])
 		compare = ncomp;
 		break;
 	case 't':
-		chartorune(&tab,ARGF());
+		chartorune(&tab, EARGF(usage()));
 		break;
 	case 'x':
 		exact++;
 		break;
 	default:
-		fprint(2, "%s: bad option %c\n", argv0, ARGC());
-		fprint(2, "usage: %s -[dfinx] [-t c] [string] [file]\n", argv0);
-		exits("usage");
+		usage();
 	} ARGEND
 	if(!iflag){
 		if(argc >= 1) {
