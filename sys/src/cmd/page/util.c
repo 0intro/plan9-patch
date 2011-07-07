7927d17d36afcc68be8678f478410e0af394bf11
David du Colombier <0intro@gmail.com>
2011-07-07 05:39:34 +0200

    patch: applied/page-label
    Set page(1)'s window label to the name of the currently displayed file.
    
    Reference: /n/sources/patch/applied/page-label
    Date: Thu Jul  7 05:39:34 CES 2011
    Signed-off-by: lyndon@orthanc.ca

--- sys/src/cmd/page/util.c
+++ sys/src/cmd/page/util.c
@@ -129,3 +129,20 @@ stdinpipe(uchar *ibuf, int in)
 	_exits(0);
 	return -1;	/* not reached */
 }
+
+/* try to update the label, but don't fail on any errors */
+void
+setlabel(char *label)
+{
+	char *s;
+	int fd;
+	
+	s = smprint("%s/label", display->windir);
+	fd = open(s, OWRITE);
+	free(s);
+	if(fd >= 0){
+		write(fd, label, strlen(label));
+		close(fd);
+	}
+	werrstr("");
+}
